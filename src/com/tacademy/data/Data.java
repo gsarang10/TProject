03a16d4954c20e4d3f441e161f0f7cc605d33dda
gsarang10 <gsarang10@gmail.com>
2017-07-21 09:53:32 +0900

    master address add

--- src/com/tacademy/data/Data.java
+++ src/com/tacademy/data/Data.java
@@ -4,6 +4,7 @@ public class Data {
 
 	private String name;
 	private int age;
+	private String masterAddress;
 	public Data() {
 		// TODO Auto-generated constructor stub
 	}
@@ -20,10 +21,18 @@ public class Data {
 	public void setAge(int age) {
 		this.age = age;
 	}
+	
+	public String getMasterAddress() {
+		return masterAddress;
+	}
+	public void setMasterAddress(String masterAddress) {
+		this.masterAddress = masterAddress;
+	}
 	@Override
 	public String toString() {
-		return "Data [name=" + name + ", age=" + age + "]";
+		return "Data [name=" + name + ", age=" + age + ", masterAddress=" + masterAddress + "]";
 	}
+	
 
 	
 
